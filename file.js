1c3f438a8f16545d7cfe6e40cb23bbb406f78d67
Tannuscl <121654576+Tannuscl@users.noreply.github.com>
2024-01-16 12:13:26 +0530

    Initial commit

---
+++ file.js
@@ -0,0 +1,12 @@
+a = require('node:fs')
+b = a.readFileSync('./a.txt', 'utf-8')
+console.log(b)
+
+a.readFile('./a.txt', 'utf-8', (error, data) =>{
+    if(error){console.log(error)}
+    else{console.log(data)}
+})
+console.log('smritiiyaaaaaa')
+console.log('yaaaaaa')
+console.log('tiiyaaaaaa')
+console.log('mritiiyaaaaaa')
