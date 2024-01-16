0ee7a3e58f61660aa98ca13eb16ff95eb3cc79df
Tannukumari8986 <tkumari930451@gmail.com>
2024-01-16 14:48:57 +0530

    Update sever.js

--- sever.js
+++ sever.js
@@ -1,8 +1,8 @@
 http = require('node:http')
 
 server = http.createServer((req,res) => {
-    res.writeHead(404)
-     res.end(" Hiee Iam tannu & this is my first node server ")
+    res.writeHead(200, {"Content-Type":"text/plain"})
+     res.end(" Hiee Iam tannu & this is my first node server")
 })
 
 server.listen(3000, ()=>{console.log('server running')})
