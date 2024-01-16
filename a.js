9be34efff2c8dc471c70063bbf5d44f1507efdac
Tannukumari8986 <tkumari930451@gmail.com>
2024-01-16 16:26:48 +0530

    just

---
+++ a.js
@@ -0,0 +1,8 @@
+mul = require('./prod')
+
+a = mul(5,8)
+console.log(a)
+console.log('my first node app')
+
+b = mul(4,8)
+console.log(b)
---
+++ prod.js
@@ -0,0 +1,5 @@
+prod = (a,b) => {
+    return a*b
+}
+
+module.exports =  prod
